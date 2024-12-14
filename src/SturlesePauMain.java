0ff15be775e3188dd8b3b66fc4d1904d658f2c64
Pau <p41stg@proton.me>
2024-12-14 22:57:00 +0100

    Se ha desarrollado la opcion [2] Borrar evento

--- src/SturlesePauMain.java
+++ src/SturlesePauMain.java
@@ -14,98 +14,116 @@ public class SturlesePauMain {
         //Creamos la lista para almacenar los eventos
         ArrayList<SturlesePauEvent> eventos = new ArrayList<>();
 
-        do{
+        do {
 
-        //Mostrar el menu de opciones
-        System.out.println("\nBienvenido a Event Planner. Selecciona una opción\n");
-        System.out.println("[1] Añadir evento");
-        System.out.println("[2] Borrar evento");
-        System.out.println("[3] Listar eventos");
-        System.out.println("[4] Marcar/desmarcar tarea de u1n evento como completado");
-        System.out.println("[5] Salir");
+            //Mostrar el menu de opciones
+            System.out.println("\nBienvenido a Event Planner. Selecciona una opción\n");
+            System.out.println("[1] Añadir evento");
+            System.out.println("[2] Borrar evento");
+            System.out.println("[3] Listar eventos");
+            System.out.println("[4] Marcar/desmarcar tarea de u1n evento como completado (INCOMPLETO)");
+            System.out.println("[5] Salir");
 
-        option= scanner.nextInt();
-        scanner.nextLine();
+            option = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (option){
-            case 1:
-                System.out.println("[1] Añadir evento");
-
-
-                //Solicitar titulo del evento obligatoriamente (no puede estar vacio)
-                String title;
-                while (true) {
-                    System.out.print("\nIntroduce el titulo del evento:");
-                    title = scanner.nextLine();
-
-                    //verificar que no este vacio
-                    if (!title.isEmpty()) {
-                        break;
-                    } else {
-                        System.out.println("El titulo no puede estar vacio");
-                    }
-                }
-
-                //Fecha del evento. Realizamos una entrada en string, convertimos a LocalDate si es posible.
-                LocalDate date = null;
-                while (date == null) {
-                    System.out.print("\nIntroduce la fecha del evento (yyyy-MM-dd):\n");
-                    String dateString = scanner.nextLine();
-
-                    try{
-                        //Intentamos convertir el String a LocalDate
-                        date = LocalDate.parse(dateString);
-                        System.out.println("Fecha validada: " + date);
-                    } catch (DateTimeParseException exception){
-                        System.out.println("Fecha no valida. Usa el formato correcto,por ejemplo (2024-01-02)");
-                    }
-                }
+            switch (option) {
+                case 1:
+                    System.out.println("[1] Añadir evento");
 
 
-                //Prioridad del evento Creamos un bucle para obtener un input valido
-                int priorityOption;
-                while (true) {
-                    System.out.print("\nIntroduce la prioridad del evento:\n 1.Alta | 2.Media | 3.Baja\n");
-                    //Verificamos que sea un numero entero
-                    if (scanner.hasNextInt()) {
-                        priorityOption = scanner.nextInt();
-                        scanner.nextLine();
+                    //Solicitar titulo del evento obligatoriamente (no puede estar vacio)
+                    String title;
+                    while (true) {
+                        System.out.print("\nIntroduce el titulo del evento:");
+                        title = scanner.nextLine();
 
-                        // Verificamos que sea 1 2 o 3
-                        if (priorityOption == 1 || priorityOption == 2 || priorityOption == 3) {
+                        //verificar que no este vacio
+                        if (!title.isEmpty()) {
                             break;
+                        } else {
+                            System.out.println("El titulo no puede estar vacio");
+                        }
+                    }
 
+                    //Fecha del evento. Realizamos una entrada en string, convertimos a LocalDate si es posible.
+                    LocalDate date = null;
+                    while (date == null) {
+                        System.out.print("\nIntroduce la fecha del evento (yyyy-MM-dd):\n");
+                        String dateString = scanner.nextLine();
+
+                        try {
+                            //Intentamos convertir el String a LocalDate
+                            date = LocalDate.parse(dateString);
+                            System.out.println("Fecha validada: " + date);
+                        } catch (DateTimeParseException exception) {
+                            System.out.println("Fecha no valida. Usa el formato correcto,por ejemplo (2024-01-02)");
+                        }
+                    }
+
+
+                    //Prioridad del evento Creamos un bucle para obtener un input valido
+                    int priorityOption;
+                    while (true) {
+                        System.out.print("\nIntroduce la prioridad del evento:\n 1.Alta | 2.Media | 3.Baja\n");
+                        //Verificamos que sea un numero entero
+                        if (scanner.hasNextInt()) {
+                            priorityOption = scanner.nextInt();
+                            scanner.nextLine();
+
+                            // Verificamos que sea 1 2 o 3
+                            if (priorityOption == 1 || priorityOption == 2 || priorityOption == 3) {
+                                break;
+
+                            } else {
+                                System.out.println("Opcion no valida. Introduce una valor valido");
+                            }
                         } else {
                             System.out.println("Opcion no valida. Introduce una valor valido");
+                            scanner.nextLine();
                         }
-                    } else {
-                        System.out.println("Opcion no valida. Introduce una valor valido");
-                        scanner.nextLine();
                     }
-                }
 
-                //Asignamos prioridad segun el valor proporcionado por el usuario
-                Priority priority;
-                if (priorityOption == 1) {
-                    priority = Priority.HIGH;
-                } else if (priorityOption == 2) {
-                    priority = Priority.MEDIUM;
-                } else {
-                    priority = Priority.LOW;
-                }
+                    //Asignamos prioridad segun el valor proporcionado por el usuario
+                    Priority priority;
+                    if (priorityOption == 1) {
+                        priority = Priority.HIGH;
+                    } else if (priorityOption == 2) {
+                        priority = Priority.MEDIUM;
+                    } else {
+                        priority = Priority.LOW;
+                    }
 
-                //Creamos el evento con los datos introducidos
-                SturlesePauEvent nuevoEvento = new SturlesePauEvent(title, date, priority);
+                    //Creamos el evento con los datos introducidos
+                    SturlesePauEvent nuevoEvento = new SturlesePauEvent(title, date, priority);
 
-                //Añadimos nuestro evento a nuestra lista
-                eventos.add(nuevoEvento);
-                System.out.println("Evento añadido!");
+                    //Añadimos nuestro evento a nuestra lista
+                    eventos.add(nuevoEvento);
+                    System.out.println("Evento añadido!");
+
+                    break;
+
+                case 2:
+                    System.out.println("[2] Borrar evento");
+                    System.out.print("\nIntroduce el titulo del evento a eliminar:");
+                    String eventoABorrar = scanner.nextLine();
+
+                    //Buscar el evento en nuestra lista
+                    boolean eventoEncontrado = false;
+                    for (int i = 0; i < eventos.size(); i++) {
+                        SturlesePauEvent evento = eventos.get(i);
+                        if (evento.getTitle().equals(eventoABorrar)) {
+                            eventos.remove(i);
+                            System.out.println("\nEvento " + eventoABorrar + " eliminado!");
+                            eventoEncontrado = true;
+                            break;
+                        }
+                    }
+                    if (!eventoEncontrado) {
+                        System.out.println("\nNo se ha encontrado ningun evento con este nombre");
+                    }
 
                 break;
-
-            case 2:
-                 System.out.println("[2] Borrar evento");
-                 break;
 
             case 3:
                  System.out.println("[3] Listar eventos");
@@ -119,12 +137,15 @@ public class SturlesePauMain {
                  }
 
                  break;
+
             case 4:
                  System.out.println("[4] Marcar/desmarcar tarea de un evento como completado");
                  break;
+
             case 5:
                  System.out.println("[5] Salir");
                  break;
+
             default:
                  System.out.println("Porfavor introduzca una opción valida");
             }
