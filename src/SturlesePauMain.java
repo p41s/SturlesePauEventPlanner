6c6fcce35c7f31591ad24264353c6882b7977d6a
Pau <p41stg@proton.me>
2024-12-14 11:54:33 +0100

    Creacion del esqueleto del menú principal

--- src/SturlesePauMain.java
+++ src/SturlesePauMain.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
 
 public class SturlesePauMain {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do{
+
+        //Mostrar el menu de opciones
+        System.out.println("\nBienvenido a Event Planner. Selecciona una opción\n");
+        System.out.println("[1] Añadir evento");
+        System.out.println("[2] Borrar evento");
+        System.out.println("[3] Listar eventos");
+        System.out.println("[4] Marcar/desmarcar tarea de un evento como completado");
+        System.out.println("[5] Salir");
+
+        option= scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option){
+            case 1:
+                System.out.println("[1] Añadir evento");
+                break;
+            case 2:
+                 System.out.println("[2] Borrar evento");
+                 break;
+            case 3:
+                 System.out.println("[3] Listar eventos");
+                 break;
+            case 4:
+                 System.out.println("[4] Marcar/desmarcar tarea de un evento como completado");
+                 break;
+            case 5:
+                 System.out.println("[5] Salir");
+                 break;
+            default:
+                 System.out.println("Porfavor introduzca una opción valida");
+            }
+
+        }while (option != 5);
     }
 }
+
+
+
